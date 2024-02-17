bce7b0cb2b1e5af98f59b2ba0d9e701a10f15f75
slikkes <slikkes@gmail.com>
2024-02-17 14:27:44 +0100

    refactor

--- src/BallBouncer.js
+++ src/BallBouncer.js
@@ -20,27 +20,27 @@ class BallBouncer {
         this.gameObjects.push(new Wall(this.gameArea.canvas.width - 10, this.gameArea.canvas.height));
         this.gameObjects.push(new Brick(origo.x, origo.y));
 
-        // create ball
-        // create bricks
-        // create player
-        // create wall
+        // TODO create bricks
+        // TODO create player
 
         const context = this.gameArea.getContext();
         setInterval(() => {
             this.gameArea.refreshGameArea();
+
             for (const gameObject of this.gameObjects) {
                 gameObject.move();
                 gameObject.draw(context);
-                if(gameObject.type !== "ball"){
+
+                if (gameObject.type !== "ball") {
 
                     const collisionAxis = gameObject.detectCollision(ball);
-                    if(collisionAxis){
+                    if (collisionAxis) {
                         //right y 490 0  460 190
                         //left y  0   0  10  160
                         //bottom x 250 250 250 280
-ball.bounce()
-                        console.log('<----[(=| collision |=)]---->', gameObject.x-ball.x,
-                            gameObject.y-ball.y,)
+                        ball.bounce()
+                        console.log('<----[(=| collision |=)]---->', gameObject.x - ball.x,
+                            gameObject.y - ball.y,)
                     }
                 }
             }
