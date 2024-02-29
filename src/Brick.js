bd44dc0731a80cc4dfdca80be1da1b4c58cad150
slikkes <slikkes@gmail.com>
2024-02-29 20:47:12 +0100

    add entity data

--- src/Brick.js
+++ src/Brick.js
@@ -1,6 +1,10 @@
 class Brick extends GameObject{
-    constructor(x, y) {
-        super('brick', x, y, 60, 30, "#75602BFF");
-    }
+    constructor(x, y, brickParams) {
+        super('brick', x, y, 60, 30, brickParams.colors[0]);
 
+        this.colors = brickParams.colors;
+        this.hp = brickParams.hp;
+        this.score = brickParams.score;
+        this.powerUps = brickParams.powerUps;
+    }
 }
---
+++ src/entities.js
@@ -0,0 +1,54 @@
+const entities = {
+  "bricks": {
+    "basic": {
+      "color": [
+        "#febe00"
+      ],
+      "hp": 1,
+      "score": 10,
+      "powerUps": []
+    },
+    "medium": {
+      "colors": [
+        "#febe00",
+        "#ff7800"
+      ],
+      "hp": 2,
+      "score": 10,
+      "powerUps": []
+    },
+    "strong": {
+      "color": [
+        "#febe00",
+        "#ff7800",
+        "#33e3da"
+      ],
+      "hp": 3,
+      "score": 10,
+      "powerUps": []
+    },
+    "ultra": {
+      "color": [
+        "#febe00",
+        "#ff7800",
+        "#33e3da",
+        "#1c93f3"
+      ],
+      "hp": 5,
+      "score": 100,
+      "powerUps": []
+    },
+    "veryUltra": {
+      "color": [
+        "#febe00",
+        "#ff7800",
+        "#33e3da",
+        "#1c93f3",
+        "#6824ff"
+      ],
+      "hp": 10,
+      "score": 500,
+      "powerUps": []
+    }
+  }
+}
